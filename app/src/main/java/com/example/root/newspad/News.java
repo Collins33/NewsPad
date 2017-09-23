7e0f16a89780a33dd8794d31c6cf21e6da80b48f
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-23 23:01:43 +0300

    I have added the model for the news

---
+++ app/src/main/java/com/example/root/newspad/News.java
@@ -0,0 +1,48 @@
+package com.example.root.newspad;
+
+/**
+ * Created by root on 9/23/17.
+ */
+
+public class News {
+    private String title;
+    private String description;
+    private String author;
+    private String image;
+    private String website;
+    private String date;
+
+    public News(String title,String description,String author,String image,String website,String date){
+        this.title=title;
+        this.description=description;
+        this.author=author;
+        this.image=image;
+        this.website=website;
+        this.date=date;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+}
