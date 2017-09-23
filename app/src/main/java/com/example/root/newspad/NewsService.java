02878a7f101598c488099102798b2e5f91db3d45
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-23 21:47:48 +0300

    I have added the service to deal with making api calls and the method to find the news

---
+++ app/src/main/java/com/example/root/newspad/NewsService.java
@@ -0,0 +1,13 @@
+package com.example.root.newspad;
+
+import javax.security.auth.callback.Callback;
+
+/**
+ * Created by root on 9/23/17.
+ */
+
+public class NewsService {
+    public static void findNews(String source, Callback callback){
+
+    }
+}
