49b07f99acae2f2ae436dc2cf885a300bdc94f3d
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-28 14:58:42 +0300

    I have added the create account page and the login page

--- app/src/main/java/com/example/root/newspad/ui/Log_In.java
+++ app/src/main/java/com/example/root/newspad/ui/Log_In.java
@@ -1,15 +1,33 @@
 package com.example.root.newspad.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.root.newspad.R;
 
-public class Log_In extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class Log_In extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(Log_In.this, Create_Account.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
