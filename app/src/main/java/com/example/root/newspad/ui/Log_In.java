f800a6aca65a87effb32f45cc8c668aad92319cb
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-29 16:26:53 +0300

    I have added the log in activity and it is functional now

--- app/src/main/java/com/example/root/newspad/ui/Log_In.java
+++ app/src/main/java/com/example/root/newspad/ui/Log_In.java
@@ -1,18 +1,42 @@
 package com.example.root.newspad.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.root.newspad.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Log_In extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
+
+    public static final String TAG = Log_In.class.getSimpleName();
+
+    @Bind(R.id.passwordLoginButton)
+    Button mPasswordLoginButton;
+    @Bind(R.id.emailEditText)
+    EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+
+
+     //instance of firebase authentication
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +44,35 @@ public class Log_In extends AppCompatActivity implements View.OnClickListener{
 
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+        auth=FirebaseAuth.getInstance();
+        //log in button
+        mPasswordLoginButton.setOnClickListener(this);
+        authStateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent=new Intent(Log_In.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            auth.removeAuthStateListener(authStateListener);
+        }
     }
 
     @Override
@@ -29,5 +82,33 @@ public class Log_In extends AppCompatActivity implements View.OnClickListener{
             startActivity(intent);
             finish();
         }
+        else if(view == mPasswordLoginButton){
+            logIn();
+        }
+    }
+
+    public void logIn(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+        //built in firebase authentication method
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                if(!task.isSuccessful()){
+                    Log.w(TAG, "signInWithEmail", task.getException());
+                    Toast.makeText(Log_In.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
