ecc90fd79f5a417c689d127e7d35a0d819cc357d
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-29 11:58:44 +0300

    I have added the authentication to firebase and the authentication was successful

--- app/src/main/java/com/example/root/newspad/ui/Create_Account.java
+++ app/src/main/java/com/example/root/newspad/ui/Create_Account.java
@@ -1,15 +1,82 @@
 package com.example.root.newspad.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.root.newspad.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Create_Account extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class Create_Account extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.createUserButton)
+    Button mCreateUserButton;
+    @Bind(R.id.nameEditText)
+    EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView)
+    TextView mLoginTextView;
+    private FirebaseAuth auth;
+    public static final String TAG = Create_Account.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create__account);
+
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+        //get instance
+        auth=FirebaseAuth.getInstance();
+    }
+    public void createNewUser(){
+        //gather credentials
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "Authentication successful");
+                }
+                else{
+                    Toast.makeText(Create_Account.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    public void onClick(View view) {
+
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(Create_Account.this, Log_In.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mCreateUserButton) {
+            createNewUser();
+        }
+
     }
 }
