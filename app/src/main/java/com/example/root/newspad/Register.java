35422dea1c2a52294f58fd88ea440b30f951a48c
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-11-05 14:53:36 +0300

    I have added the account registration page

--- app/src/main/java/com/example/root/newspad/Register.java
+++ app/src/main/java/com/example/root/newspad/Register.java
@@ -1,13 +1,29 @@
 package com.example.root.newspad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Register extends AppCompatActivity {
+import com.example.root.newspad.ui.Create_Account;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+@Bind(R.id.button3) Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        mButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        Intent intent=new Intent(getApplicationContext(), Create_Account.class);
+        startActivity(intent);
+        finish();
     }
 }
