4e917442858411c772c04ca4037436f67d6251c5
Collins njau <collinsnjau39@gmail.com>
2018-01-27 22:07:39 +0300

    I have added the view function for all the buttons to choose the category

--- app/src/main/java/com/example/root/newspad/Category.java
+++ app/src/main/java/com/example/root/newspad/Category.java
@@ -2,12 +2,38 @@ package com.example.root.newspad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Category extends AppCompatActivity {
+    @Bind(R.id.button2) Button mSports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        ButterKnife.bind(this);
+    }
+
+
+    public void sports(View view) {
+        Toast toast=Toast.makeText(getApplicationContext(),"sports",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void technology(View view) {
+    }
+
+    public void politics(View view) {
+    }
+
+    public void finance(View view) {
+    }
+
+    public void health(View view) {
     }
 }
