4f301c95a68eeae577c23039602ed5f9ed64bcd3
Collins njau <collinsnjau39@gmail.com>
2018-01-29 22:21:27 +0300

    I have added the code to get the data from categories and and the activity with the news based on the activity

--- app/src/main/java/com/example/root/newspad/Category.java
+++ app/src/main/java/com/example/root/newspad/Category.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.root.newspad.ui.NewsListActivity;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Category extends AppCompatActivity {
+    public static final String TAG = Category.class.getSimpleName();
     @Bind(R.id.button2) Button mSports;
 
     @Override
@@ -23,32 +26,32 @@ public class Category extends AppCompatActivity {
 
     public void sports(View view) {
         Intent intent=new Intent(getApplicationContext(),categorynews.class);
-        intent.putExtra("sports","sports");
+        intent.putExtra(TAG ,"sports");
         startActivity(intent);
     }
 
     public void technology(View view) {
         Intent intent=new Intent(getApplicationContext(),categorynews.class);
-        intent.putExtra("sports","sports");
+        intent.putExtra(TAG ,"technology");
         startActivity(intent);
     }
 
     public void politics(View view) {
         Intent intent=new Intent(getApplicationContext(),categorynews.class);
-        intent.putExtra("sports","sports");
+        intent.putExtra(TAG ,"politics");
         startActivity(intent);
     }
 
     public void finance(View view) {
         Intent intent=new Intent(getApplicationContext(),categorynews.class);
-        intent.putExtra("sports","sports");
+        intent.putExtra(TAG ,"finance");
         startActivity(intent);
 
     }
 
     public void health(View view) {
         Intent intent=new Intent(getApplicationContext(),categorynews.class);
-        intent.putExtra("sports","sports");
+        intent.putExtra(TAG ,"health");
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/root/newspad/categorynews.java
+++ app/src/main/java/com/example/root/newspad/categorynews.java
@@ -1,7 +1,9 @@
 package com.example.root.newspad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class categorynews extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class categorynews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorynews);
+        //getting the intent text
+        Intent intent= getIntent();
+        String message=intent.getStringExtra(Category.TAG);
+
+        //test toast
+        Toast toast= Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG);
+        toast.show();
     }
 }
